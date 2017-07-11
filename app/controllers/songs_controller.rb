3cb789c87d0a266512a42971975236c473c39325
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-11 07:52:49 +0200

    Create show and new song pages

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,25 @@
 class SongsController < ApplicationController
   def index
    @songs = Song.all
- end
+  end
+
+  def new
+  @song = Song.new
+  end
+
+  def show
+  @song = Song.find(params[:id])
+  end
+
+  def create
+  song_params = params.require(:song).permit(:music_genre, :producer, :image_url)
+
+  @song = Song.new(song_params)
+
+  if @song.save
+     redirect_to @song
+  else
+     render 'new'
+  end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "songs#index"
 
     get "songs" => "songs#index"
+    get 'songs/new' => "songs#new", as: :new_song
+    get "songs/:id" => "songs#show", as: :song
+
+    post "songs" => "songs#create"
+
+
 end
