37cc7b094afdcdfc7013eb25c04541c73f999f15
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-11 08:51:21 +0200

    Add song validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,14 @@
 class Song < ApplicationRecord
   def self.order_by_music_genre
       order(:music_genre)
-    end
+  end
+
+  class Song
+  validates :music_genre, presence: true
+  validates :music_genre, uniqueness: true
+
+  validates :producer, presence: true
+  validates :producer, uniqueness: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
 
 end
