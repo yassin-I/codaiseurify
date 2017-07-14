facac9039578c3c102c2abb0fd101e54187bfe09
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-14 15:45:00 +0200

    delete songs from artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,34 +1,38 @@
 class SongsController < ApplicationController
 
 
-def new
-  @artist = Artist.find(params[:artist_id])
+  def new
+    @artist = Artist.find(params[:artist_id])
 
-  @song = @artist.songs.new
+    @song = @artist.songs.new
+  end
+
   def create
-    @song = Song.new(params[:song].permit(params[:artist_id])
+    @song = Song.new(song_params)
     if @song.save
       redirect_to @song
     else
       render 'new'
     end
   end
+
   def edit
     @song = Song.find(params[:artist_id])
   end
 
-def destroy
-  @song = Song.find(params[:artist_id])
+  def destroy
+    @song = Song.find(params[:id])
 
-  if @song.destroy
-    redirect_to @song.artist
-  else
-    redirect_to @song.artist
+    if @song.destroy
+      redirect_to '/', notice: "destroyed"
+    else
+      redirect_to @song.artist
+    end
   end
-end
 
-def song_params
- params.
- require(:song).permit(:name, :artist_id)
-end
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id)
+  end
 end
